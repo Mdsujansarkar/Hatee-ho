405d387e277bd4fe6a4b828b6120095adf816e21
Sujan Miya <sujansarkar786@gmail.com>
2022-01-31 21:06:34 +0600

    Update Taskv3.php
    ok

--- src/Task/Taskv3.php
+++ src/Task/Taskv3.php
@@ -6,35 +6,9 @@ use App\Taskv1;
 
 class Taskv3
 {
-    // public $arrayList = [];
-    // public $arraySecound = [1,4,9];
+
     public function task_three()
     {
-
-        // $array = [];
-
-        //  $arraySecound = [1,4,9];
-
-        // for($i = 1; $i<=10; $i++ )
-        // {
-        //      $array[$i] = $i . PHP_EOL;
-
-
-        // }
-        // print_r($array);
-        // print_r($arraySecound);
-        // print_r($result = array_intersect($array, $arraySecound));
-        // if($result){
-        //     print_r($result);
-        //     }
-
-        // $array1 = array(2,5,3);
-        // $array2 = array(5,2,3);
-        // if($array1 === array_intersect($array1, $array2) && $array2 === array_intersect($array2, $array1)) {
-        //     echo 'Equal';
-        // } else {
-        //     echo 'Not equal';
-        // }
 
         $arrayStore = [];
         $arrayAnother =[];
@@ -54,13 +28,7 @@ class Taskv3
             }
         }
         echo implode('-',$arrayStore);
-        // $another = array_intersect($array,$arrayAnother);
-
-        // print_r($another);
-
-        // $result=array_diff_key($arrayAnother,$arraySecound);
-
-        // print_r($result);
+    
     }
 }
 
